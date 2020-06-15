00db155ef43458cb7bdfd86e4e657c04c6985f24
hm-y <hmy0hmy@gmail.com>
2020-06-15 15:01:41 -0400

    Add .apk file

--- lib/home.dart
+++ lib/home.dart
@@ -94,7 +94,7 @@ class _HomeState extends State<Home>{
         child: Text("I can't decide")
       ),
       SizedBox(height: 10),
-      Text("You voted " + _numberOfVotes.toString() + " ideas")
+      Text("You voted " + (_numberOfVotes-1).toString() + " ideas")
     ];
   }
 
